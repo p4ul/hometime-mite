e80c5bb27b60251b65557fb715f547bba1a647c1
Paul Sinclair <p4ul.sincl4ir@gmail.com>
2012-04-20 08:50:46 +1200

    changed formating slightly

--- hometime-mite.user.js
+++ hometime-mite.user.js
@@ -13,29 +13,27 @@
 
 var main = function () {
 	'use strict';
-	var version = '0.0.1';
+	var version = '0.0.1',
+		goalMinutes = 7*60+30;
 	
 	//create a button
 	
 	$('.side:last').append( $('<span>').html($('<a id="time_left">').addClass('button').html('show time')) );
-		$('.side:last').append( $('<br>').css('clear','both' ) );
-	$('.side:last').append( $('<span id="stats">').css({float:'left',height:'50px','margin-left':'15px'}).html('') );
+	$('.side:last').append( $('<br>').css('clear','both' ) );
+	$('.main.second').after( $('<div id="stats">').addClass('main').css({float:'left',height:'50px',width:'77%','background-color':''}).html('') );
 	
 	//new Date(year, month, day, hours, minutes, seconds, milliseconds)
 	//d.getHours();
 	
-	var goalMinutes = 7*60+30;
+
 	
-	function goalMinutesLeft(){
-	
-		
+	function getGoalMinutesLeft(){
 		return goalMinutes-getTotalTime();
 	}
 	
 	function getTotalTime(){
 		var timeArray = $('#minutes_sum').text().split(':'),
-			totalTime = timeArray[0] * 60 + parseInt(timeArray[1],10);
-			
+			totalTime = timeArray[0] * 60 + parseInt(timeArray[1],10);			
 		return totalTime;		
 	}
 	
@@ -60,12 +58,15 @@ var main = function () {
 	}
 	
 	$('#time_left').live('click',function(){
-				var status = getStatus(homeTime(), goalMinutesLeft());
+				var status = getStatus(homeTime(), getGoalMinutesLeft());
 				console.dir(status);
 				$('#stats').html('');
-				$('#stats').append($('<p>').html("remaining minutes " + status.remainingMins));
-				$('#stats').append($('<p>').html("goal minutes " + status.goalMinutesLeft));
-				$('#stats').append($('<p>').html("spare minutes " + status.spareMinutes));
+				$('#stats').append("remaining minutes in day "); 
+				$('#stats').append( $('<em>').html(status.remainingMins));
+				$('#stats').append(", goal minutes left "); 
+				$('#stats').append( $('<em>').html(status.goalMinutesLeft));
+				$('#stats').append(", spare minutes "); 
+				$('#stats').append( $('<em>').html(status.spareMinutes));
 	});
 		
 };
